12ffe8f77be59c075491c65c8124e0ddca98b3fb
leo <leoyenschen@gmail.com>
2017-05-09 17:23:23 +0800

    add into database

--- trips/views.py
+++ trips/views.py
@@ -10,7 +10,23 @@ import os
 import json
 
 from apscheduler.schedulers.background import BackgroundScheduler
-scheduler = BackgroundScheduler()
+from pytz import utc
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
+
+jobstores = {
+
+    'default': SQLAlchemyJobStore(url='sqlite:///db.sqlite3')
+}
+executors = {
+    'default': ThreadPoolExecutor(20),
+    'processpool': ProcessPoolExecutor(5)
+}
+job_defaults = {
+    'coalesce': False,
+    'max_instances': 3
+}
+scheduler = BackgroundScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone=utc)
 scheduler.start()
 
 def test_1():
